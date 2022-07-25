531d5314b82563aaa144ec3b644d4150466fd148
Mustafa Ayaz <ayz.mstafa@gmail.com>
2022-07-25 19:54:00 +0300

    globalexception

---
+++ Exceptions/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Http.Features;
+
+namespace Exceptions.Middleware;
+
+public class ExceptionHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+    public ExceptionHandlingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext httpContext)
+    {
+        try
+        {
+            await _next.Invoke(httpContext);
+        }
+        catch (Exception e)
+        {
+            await HandleException(httpContext.Response, e);
+        }
+    }
+
+    private static async Task HandleException(HttpResponse httpResponse, Exception exception)
+    {
+        httpResponse.Headers.Add("Exception-Type", exception.GetType().Name);
+        httpResponse.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = "Huston we have a problem";
+        httpResponse.StatusCode = (int)HttpStatusCode.BadRequest;
+        await httpResponse.WriteAsync(exception.Message).ConfigureAwait(false);
+
+    }
+}
+
+public static class AppBuilderExtensions
+{
+    public static void UseCustomExceptionHandler(this IApplicationBuilder app)
+    {
+        app.UseMiddleware<ExceptionHandlingMiddleware>();
+    }
+}
